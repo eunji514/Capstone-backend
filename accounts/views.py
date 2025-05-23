d48e2c7bf1b38ce4080e8c375e6116af1db3323a
전은지 <jeon-eunji@jeon-eunjiui-MacBookAir.local>
2025-05-23 15:29:34 +0900

    create views.py

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .serializers import SignupSerializer
 
-# Create your views here.
+class SignupUserInfoView(APIView):
+    def post(self, request):
+        serializer = SignupSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            return Response(
+                {
+                    "message": "기본 정보 저장 완료",
+                    "email": user.email  # buddyinfo 단계로 넘길 때 필요
+                },
+                status=status.HTTP_201_CREATED
+            )
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
