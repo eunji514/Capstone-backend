4df01013f8bc44d507d04188a7ad4552b06eac74
전은지 <jeon-eunji@jeon-eunjiui-MacBookAir.local>
2025-05-23 15:49:50 +0900

    edit serializers.py-add buddyprofileserializer

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import User
+from .models import User, BuddyProfile
 from .validators import validate_strong_password
 
 class SignupSerializer(serializers.ModelSerializer):
@@ -11,3 +11,40 @@ class SignupSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
         return User.objects.create_user(**validated_data)
+
+class BuddyProfileSerializer(serializers.ModelSerializer):
+    interest = serializers.ListField(child=serializers.CharField())
+    language = serializers.ListField(child=serializers.CharField())
+    purpose = serializers.ListField(child=serializers.CharField())
+
+    email = serializers.EmailField(write_only=True)  
+
+    class Meta:
+        model = BuddyProfile
+        fields = ['email', 'interest', 'language', 'purpose', 'matching_type']
+
+    def create(self, validated_data):
+        email = validated_data.pop('email')
+        try:
+            user = User.objects.get(email=email)
+        except User.DoesNotExist:
+            raise serializers.ValidationError({"email": "해당 이메일의 사용자가 존재하지 않습니다."})
+
+        interest = ",".join(validated_data.pop('interest'))
+        language = ",".join(validated_data.pop('language'))
+        purpose = ",".join(validated_data.pop('purpose'))
+
+        return BuddyProfile.objects.create(
+            user=user,
+            interest=interest,
+            language=language,
+            purpose=purpose,
+            matching_type=validated_data['matching_type']
+        )
+
+    def to_representation(self, instance):
+        rep = super().to_representation(instance)
+        rep['interest'] = instance.interest.split(',')
+        rep['language'] = instance.language.split(',')
+        rep['purpose'] = instance.purpose.split(',')
+        return rep 
