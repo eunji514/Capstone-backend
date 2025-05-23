0d2b0f4311b99296635c99756ca6017bb277d694
전은지 <jeon-eunji@jeon-eunjiui-MacBookAir.local>
2025-05-23 15:52:47 +0900

    add buddyinfoview

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,7 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from .serializers import SignupSerializer
+from .serializers import SignupSerializer, BuddyProfileSerializer
 
 class SignupUserInfoView(APIView):
     def post(self, request):
@@ -13,6 +13,18 @@ class SignupUserInfoView(APIView):
                     "message": "기본 정보 저장 완료",
                     "email": user.email  # buddyinfo 단계로 넘길 때 필요
                 },
+                status=status.HTTP_201_CREATED
+            )
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class SignupBuddyInfoView(APIView):
+    def post(self, request):
+        serializer = BuddyProfileSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(
+                {"message": "버디 정보 저장 완료. 회원가입이 완료되었습니다."},
                 status=status.HTTP_201_CREATED
             )
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
