365d4a58cf40b9082024ad2470dbcc872b6438f1
전은지 <jeon-eunji@jeon-eunjiui-MacBookAir.local>
2025-05-23 16:26:47 +0900

    edit signup url

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import SignupUserInfoView, SignupBuddyInfoView
 
 urlpatterns = [
-    path('signup/userinfo/', SignupUserInfoView.as_view(), name='signup-userinfo'),
-    path('signup/buddyinfo/', SignupBuddyInfoView.as_view(), name='signup-buddyinfo'),
+    path('userinfo/', SignupUserInfoView.as_view(), name='signup-userinfo'),
+    path('buddyinfo/', SignupBuddyInfoView.as_view(), name='signup-buddyinfo'),
 ]
