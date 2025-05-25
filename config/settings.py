c5fb0b575a0258c46019b960aac8f51634c23001
전은지 <jeon-eunji@jeon-eunjiui-MacBookAir.local>
2025-05-25 15:44:55 +0900

    페이지네이션

--- config/settings.py
+++ config/settings.py
@@ -133,7 +133,9 @@ REST_FRAMEWORK = {
     ],
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.AllowAny',  
-    ]
+    ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
 }
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
