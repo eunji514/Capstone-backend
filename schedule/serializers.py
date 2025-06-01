7d51143497daac8b507eae6e1f55903d10c7825a
전은지 <jeon-eunji@jeon-eunjiui-MacBookAir.local>
2025-06-02 01:56:25 +0900

    날짜별 일정 리스트&게시글 이동

--- schedule/serializers.py
+++ schedule/serializers.py
@@ -4,7 +4,9 @@ from .models import CalendarEvent
 class CalendarEventSerializer(serializers.ModelSerializer):
     student_council_name = serializers.CharField(source='student_council.name')
     student_council_color = serializers.CharField(source='student_council.color')
-    
+    post_id = serializers.IntegerField(source='post.id', read_only=True) 
+
+
     class Meta:
         model = CalendarEvent
-        fields = ['id', 'title', 'location', 'start', 'end', 'student_council_name', 'student_council_color']
+        fields = ['id', 'title', 'location', 'start', 'end', 'student_council_name', 'student_council_color', 'post_id']
--- schedule/views.py
+++ schedule/views.py
@@ -2,6 +2,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import CalendarEvent
 from .serializers import CalendarEventSerializer
+from datetime import datetime
 
 class MonthlyCalendarView(APIView):
     def get(self, request):
@@ -16,3 +17,15 @@ class MonthlyCalendarView(APIView):
             events = events.filter(student_council_id=council_id)
 
         return Response(CalendarEventSerializer(events, many=True).data)
+
+class EventsByDateView(APIView):
+    def get(self, request):
+        date_str = request.query_params.get("date")  # 'YYYY-MM-DD'
+        try:
+            date = datetime.strptime(date_str, "%Y-%m-%d").date()
+        except (TypeError, ValueError):
+            return Response({"error": "날짜 형식이 올바르지 않습니다."}, status=400)
+
+        events = CalendarEvent.objects.filter(start__date=date)
+        serializer = CalendarEventSerializer(events, many=True)
+        return Response(serializer.data)
